0a878f09699aaaa977d85c72514e09719cb0905f
IvanMarte <ivanbiasfx@gmail.com>
2023-12-29 17:44:41 -0600

    Create the Register Forms

---
+++ js/estilos.js
@@ -0,0 +1,7 @@
+// Se realizo para dar una modificacion de estilos al tipo input
+
+var input = document.getElementsByClassName('inputJs');
+
+for(var i=0; i<input.length; i++){
+    input[i].style.padding = '14px';
+}
--- view/registro.view.php
+++ view/registro.view.php
@@ -7,8 +7,9 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Montserrat:wght@700&family=Raleway:ital,wght@0,400;0,500;0,800;1,300&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+    <script src="https://kit.fontawesome.com/6bbb91872d.js" crossorigin="anonymous"></script>
 
+    
     
     <title>Registro</title>
 
@@ -19,9 +20,34 @@
         <h1 class="titulo">Registro</h1>
         <hr class="border">
         
+        <!-- Evitar que nos inyecten código  -->
+        <!-- El name nos va a ayudar a despues enviar el formulario  -->
+        <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="POST" class="formulario" name="login"> 
+            <div class="form-group">
+                <!-- Las Clases van a traer el icono de FontAwesome -->
+                <i class="icono izquierda fa-solid fa-user"></i><input  type="text" name="usuario" class="usuario inputJs" placeholder="Usuario">
+            </div>
 
+            <div class="form-group">
+                <i class="icono izquierda fa-solid fa-lock"></i><input type="password" name="password" class="password inputJs" placeholder="Contraseña  ">
+            </div>
+            <div class="form-group">
+                <i class="icono izquierda fa-solid fa-lock"></i><input type="password" name="password2" class="password_btn inputJs" placeholder="Repetir Contraseña">
+                <!-- Se usa el placeholder para enviar la información directamente al name del Formulario -->
+                <i class="submit-btn fa-solid fa-arrow-right" onclick="login.submit()"></i>
+            </div>
+        </form>   
+        
+        <p class="texto-registrate">
+            ¿Ya tienes cuenta=
+
+            <a href="login.php">Iniciar Sesion</a>
+        </p>
     </div>
 
 
+
 </body>
-</html>
+
+    <script src="js/estilos.js"></script>
+</html> 
